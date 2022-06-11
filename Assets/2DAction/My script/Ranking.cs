69ca807884ef4f5fb8cfe8aea26de84f3f17bae5
sinzitoyama <yamat422@gmail.com>
2022-06-12 00:01:06 +0900

    playerprefs完成

--- Assets/2DAction/My script/Ranking.cs
+++ Assets/2DAction/My script/Ranking.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Ranking : MonoBehaviour
 {
+    [SerializeField] GameObject tim;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+       string trm = PlayerPrefs.GetString("time","999");
+        tim.GetComponent<Text>().text = trm;
+        PlayerPrefs.DeleteAll();
     }
 
     // Update is called once per frame
--- Assets/2DAction/My script/Timer.cs
+++ Assets/2DAction/My script/Timer.cs
@@ -2,23 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Timer : MonoBehaviour
 {
-    [SerializeField] float tr = 0f;
-    GameObject trm;
+    public GameObject trm;
+    private bool stop = false;
+    [SerializeField] string Scene = "Result";
+
+    public string nowTime;
+
+    private float _testCountTime;
+
+    string time;
     //private Text te;
     // Start is called before the first frame update
     void Start()
     {
         gameObject.GetComponent<Text>();
         trm = GameObject.Find("_timer");
+        _testCountTime = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //tr += Time.deltaTime;
-        trm.GetComponent<Text>().text = Time.time.ToString("F1");
+        if(stop == false)
+        {
+            _testCountTime += Time.deltaTime;
+            //trm.GetComponent<Text>().text = Time.time.ToString("F1");
+            trm.GetComponent<Text>().text = _testCountTime.ToString("F1");
+            nowTime = _testCountTime.ToString("F1");
+        }
+       
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(gameObject.tag =="Goal")
+        {
+            stop = true;
+            PlayerPrefs.SetString("time", nowTime);
+            PlayerPrefs.Save();
+            Debug.Log(PlayerPrefs.GetString("time") + "Save");
+            SceneManager.LoadScene(Scene);
+        }
+
     }
 }
